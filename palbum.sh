92c9c9f26ed9201a3e17a5ab0dce0e14be9b2918
Benjamin <benjamin.teyssier@etu.emse.fr>
2021-11-02 17:24:34 +0100

    Creation repertoires jours et fonction deplacement

--- palbum.sh
+++ palbum.sh
@@ -10,12 +10,50 @@ CheckInputDirectory(){
 			echo "Directory "$inputDirectory" DOES NOT exists."
 			return
 	fi
-	numberOfImages=$(ls "$inputDirectory" -a | grep ".png\|.jpg" | wc -l)
+	numberOfImages=$(ls "$inputDirectory" -a | grep ".png\|.PNG\|.jpg\|.JPG" | wc -l)
 	if [[ $numberOfImages -eq 0 ]]
 		then
 			echo "There is no image files in "$inputDirectory"."
 			return
 	fi
+}
+
+CreateYearRepositories(){
+	photoYears=$(exiftool -T -DateTimeOriginal "$inputDirectory" -d %Y | sort --unique)
+	rm "$outputDirectory"/index.html
+	for y in $photoYears
+	do
+		mkdir -p "$outputDirectory"/$y
+		if [[ -f "$outputDirectory"/$y/index.html ]]
+			then
+				rm "$outputDirectory"/$y/index.html
+		fi
+		touch "$outputDirectory"/$y/index.html
+		linkToRepository=$outputDirectory/$y/index.html
+		echo "<a href="$linkToRepository">$y</a><br></br>">>index.html
+	done
+}
+
+CreateDayDirectories(){
+	for y in $photoYears
+	do
+		photoDays=$(exiftool -T -DateTimeOriginal "$inputDirectory" -d %Y_%m_%d | sort --unique | grep $y)
+		for d in $photoDays
+		do
+			mkdir -p "$outputDirectory"/$y/$d
+			mkdir -p "$outputDirectory"/$y/$d/.thumbs
+		done
+	done
+}
+
+MovePhotosToDirectories(){
+	for img in $(ls "$inputDirectory" -a | grep ".png\|.PNG\|.jpg\|.JPG")
+	do
+		photoYear=$(exiftool -T -DateTimeOriginal "$inputDirectory"/$img -d %Y)
+		photoDay=$(exiftool -T -DateTimeOriginal "$inputDirectory"/$img -d %Y_%m_%d)
+		photoName="${photoDay} ${img}"
+		cp -a "$inputDirectory"/$img "$outputDirectory"/"$photoYear"/"$photoDay"/"$photoName"
+	done
 }
 
 inputDirectory="$1"
@@ -28,3 +66,6 @@ if [[ ! -d "$outputDirectory" ]]
 		mkdir "$outputDirectory"
 		touch "$outputDirectory"/index.html
 fi
+CreateYearRepositories
+CreateDayDirectories
+MovePhotosToDirectories
