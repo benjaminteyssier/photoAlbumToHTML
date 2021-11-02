18ec11c68f5cba29e8ee2d1b94457b2d44b7a0f4
Benjamin <benjamin.teyssier@etu.emse.fr>
2021-11-02 10:39:02 +0100

    Ajout verif repertoire entree et creation repertoire sortie

--- palbum.sh
+++ palbum.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
-Help()
-{
+Help(){
    cat README.md
 }
 
-Help
+CheckInputDirectory(){
+	if [[ ! -d $inputDirectory ]]
+		then
+			echo "Directory "$inputDirectory" DOES NOT exists."
+			return
+	fi
+	numberOfImages=$(ls "$inputDirectory" -a | grep ".png\|.jpg" | wc -l)
+	if [[ $numberOfImages -eq 0 ]]
+		then
+			echo "There is no image files in "$inputDirectory"."
+			return
+	fi
+}
+
+inputDirectory="$1"
+outputDirectory="$2"
+
+CheckInputDirectory
+
+if [[ ! -d "$outputDirectory" ]]
+	then 
+		mkdir "$outputDirectory"
+		touch "$outputDirectory"/index.html
+fi
